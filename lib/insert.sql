69ff033b766e75fe2f99486dbb689c7b99ca8ee0
angpace <108838753+angpace@users.noreply.github.com>
2022-11-03 15:04:00 -0400

    commit tree fdc4cc3ed88a4ee3248ff7da46a43a60b1109376

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Mr. Chocolate", 12, "M", "brown", "nice", "1"), 
+("Rowdy", 10, "M", "brown", "super nice", "1"), 
+("Tabitha", 3, "F", "brown", "nice", "1"),
+("Sergeant Brown", 5, "M", "brown", "bad", "1"), 
+("Melissa", 2, "M", "brown", "Good", "1"), 
+("Grinch", 1, "M", "brown", "nice", "0"),
+("Wendy", 12, "M", "brown", "nice", "1"), 
+(NULL, 12, "M", "brown", "nice", "1")
